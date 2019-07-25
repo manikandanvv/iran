b67da30579b70ec60485f969fe220c03bcd419cf
manikandanvv <46994685+manikandanvv@users.noreply.github.com>
2019-07-25 14:17:51 +0530

    Add files via upload

---
+++ login_angular 7/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+import { version } from 'punycode';
+import * as $ from 'jquery';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  {
+  title = 'my-nw-app';
+}
